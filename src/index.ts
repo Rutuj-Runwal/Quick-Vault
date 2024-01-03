94f275e1aab0c612987bf76c399172fa4bce8d5a
Rutuj Runwal <rutujrunwal001@gmail.com>
2024-01-03 18:32:04 +0530

    Feat: Dropped argparse

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,10 @@
 #!/usr/bin/env node
-
-import {ArgumentParser} from 'argparse';
 import Store from './store.js';
 
 const QUIK_COMMANDS = ["add","edit","get","remove","dump","clear"];
 const QUIK_VAULT_PATH = "./";
 
 const quikVault = new Store(QUIK_VAULT_PATH);
-
-const parser = new ArgumentParser();
-const subparsers = parser.add_subparsers({title:"Quik Vault actions"});
-parser.add_argument('-v', '--version', { action: 'version','version':'1.0.0' });
-
-const add_parser = subparsers.add_parser("add", {help:"Add key-value pair to Quik Vault"});
-const get_parser = subparsers.add_parser("get", {help:"Get value for a particular key from Quik Vault"});
-const edit_parser = subparsers.add_parser("edit", {help:"Get value for a particular key from Quik Vault"});
-
-//@ts-ignore
-add_parser.add_argument({
-    dest:"quikcommands",
-    nargs: 2,
-    help: "Aceepts a key value pair",
-});
-add_parser.set_defaults({func:add});
-
-//@ts-ignore
-get_parser.add_argument({
-    dest:"quikcommands",
-    nargs: 1,
-    help: "Access the value based on key",
-});
-get_parser.set_defaults({func:get});
-
-//@ts-ignore
-edit_parser.add_argument({
-    dest:"quikcommands",
-    nargs: 2,
-    help: "Edit the value based on key",
-});
-edit_parser.set_defaults({func:edit});
-
-const args = parser.parse_args();
-
-try{
-    args.func(args.quikcommands);
-}catch{
-    parser.print_help();
-}
-
 
 function add([key,val]:[string,string]){
     if(quikVault.exists(key)){
